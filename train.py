b6229b316180442e74e06c9dbe1540e9d4bd8a95
yxing1_cube3 <xyf.jim@gmail.com>
2017-04-16 21:22:51 -0400

    change feature dimension

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ def main():
     # load val dataset to print out bleu scores every epoch
     val_data = load_coco_data(data_path='./data', split='val')
 
-    model = CaptionGenerator(word_to_idx, dim_feature=[196, 512], dim_embed=512,
+    model = CaptionGenerator(word_to_idx, dim_feature=[64, 2048], dim_embed=512,
                                        dim_hidden=1024, n_time_step=16, prev2out=True, 
                                                  ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
 
